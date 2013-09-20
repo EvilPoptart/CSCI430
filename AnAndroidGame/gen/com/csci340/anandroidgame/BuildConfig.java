0d93ddc052dbbc18f1738037da9a072001cba3d9
EvilPoptart <mike57343@gmail.com>
2013-09-19 23:46:19 -0700

    Fixed package
    changed from 340 to 430 in package

--- AnAndroidGame/gen/com/csci340/anandroidgame/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.csci340.anandroidgame;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
---
+++ AnAndroidGame/gen/com/csci430/anandroidgame/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.csci430.anandroidgame;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
