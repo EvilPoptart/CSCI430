c4f34cbe15bad5cdae35621845629cdf17458f8e
DeusExVerbum <cweaver8@mail.csuchico.edu>
2013-10-12 14:57:21 -0700

    Merge conflict resolution

--- AnAndroidGame/src/com/csci430/anandroidgame/FullscreenActivity.java
+++ AnAndroidGame/src/com/csci430/anandroidgame/FullscreenActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.widget.Button;
 import android.view.*;
+import android.view.View.OnTouchListener;
 import android.media.*;
 
 
@@ -49,6 +51,7 @@ public class FullscreenActivity extends Activity {
 		buttonJump1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) 
 			{            
+				Log.i("Buttons", "Jump (left)");
         		jumpsound.start();
             	Global.jump();
 			}
@@ -60,6 +63,7 @@ public class FullscreenActivity extends Activity {
 		buttonJump2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) 
 			{
+				Log.i("Buttons", "Jump (right)");
         		jumpsound.start();
             	Global.jump();
 			}
@@ -68,21 +72,35 @@ public class FullscreenActivity extends Activity {
 		/*
 		 * Increases player's rightward movement speed up to the MAX_SPEED.
 		 */
-		buttonRight.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) 
-			{
-            	Global.runRight();
+		buttonRight.setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					Log.i("Buttons", "Right DOWN");
+					Global.startRunningRight();
+				}
+				else if (event.getAction() == MotionEvent.ACTION_UP) {
+					Log.i("Buttons", "Right UP");
+					Global.setRunning(false);
+				}
+				return false;
 			}
         });
 		
 		/*
 		 * Increases player's leftward movement speed up to the MAX_SPEED.
 		 */
-		buttonLeft.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) 
-			{	
-            	Global.runLeft();
-            }
+		buttonLeft.setOnTouchListener(new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					Log.i("Buttons", "Left DOWN");
+					Global.startRunningLeft();
+				}
+				else if (event.getAction() == MotionEvent.ACTION_UP) {
+					Log.i("Buttons", "Left UP");
+					Global.setRunning(false);
+				}
+				return false;
+			}
         });	
 	}
     
